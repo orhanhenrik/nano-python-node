f801e498a94c04426012ed50a732d65b2df911a7
Orhan Hirsch <orhanhenrik@gmail.com>
2018-02-11 19:38:38 +0100

    add key generation from seed

--- util/numbers.py
+++ util/numbers.py
@@ -1,3 +1,5 @@
+from pure25519_blake2b.ed25519_oop import SigningKey
+
 from util.crypto import blake2b_hash
 
 account_loopup = "13456789abcdefghijkmnopqrstuwxyz"
@@ -50,6 +52,12 @@ def account_to_public_key(account: str) -> bytes:
     return public_key
 
 
+def deterministic_key(seed: bytes, index: int):
+    private_key = blake2b_hash(seed+index.to_bytes(4, byteorder="big"), digest_size=32)
+    public_key = SigningKey(private_key).get_verifying_key().to_bytes()
+    return private_key, public_key
+
+
 if __name__ == '__main__':
     public_key = bytes.fromhex('00002C972CAB7164C44E237AD0AA4A54D110006CA22715D2DB26ED180365E4CB')
     account = 'xrb_11117kdkscujem46wautt4o6no8j4118saj94qbfpbqf513pds8dqm4e4cfn'
@@ -62,3 +70,10 @@ if __name__ == '__main__':
     print(public_key.hex())
     assert account_to_public_key(account) == public_key
 
+    seed = bytes.fromhex('91C6120B51D2244F912A9F94BF60588712F10151FC57D7F33697D064C91290A8')
+
+    keys0 = (bytes.fromhex('74E988A492618A05463233D92A975DBD697FD9EC9C584FACC280C82AB5B8A8D8'), bytes.fromhex('6B012D2E8D389D52DAF89FFC38D23279D754B65C700DB0DB20133C8314A84686'))
+    assert keys0 == deterministic_key(seed, 0)
+
+    keys9 = (bytes.fromhex('16923523074D88994D97F30858E626FA80587B71A10099A0FBC899240F526D5C'), bytes.fromhex('A6A1CE1BCE9D5910FD68533431E3B3FA89855ACDB6E8DFA55622C32963CBD4FD'))
+    assert keys9 == deterministic_key(seed, 9)
